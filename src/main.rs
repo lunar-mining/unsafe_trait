18d843a7ebb43ceaece6cabb48e16192d209ed87
lunar-mining <your_email@abc.example>
2024-04-30 08:48:18 +0200

    commit working example

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
 
-struct Bar {
-    dyn_hashmap: Mutex<HashMap<&'static str, Arc<dyn Foo>>>,
+struct Bar<R> {
+    dyn_hashmap: Mutex<HashMap<&'static str, Arc<dyn Foo<R>>>>,
 }
 
 // We cannot use generic type parameters here.
@@ -10,8 +10,8 @@ struct Bar {
 // For a trait to be "object safe" it needs to allow building
 // a vtable to allow the call to be resolvable dynamically:
 // https://doc.rust-lang.org/reference/items/traits.html#object-safety
-trait Foo {
-    fn do_something<R>(&self, bar: &mut R);
+trait Foo<R> {
+    fn do_something(&self, bar: &mut R);
 }
 
 fn main() {
